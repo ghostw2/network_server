2e8126c02be9821ce0a4c9c5d987a5a49b60f917
menri <m.bardhi@wisch4web.de>
2024-04-29 23:53:37 +0200

    showip

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+int  main(){
+    printf("Hello from c");
+    return 0;
+}
